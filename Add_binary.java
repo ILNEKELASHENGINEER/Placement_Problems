ae8ba483a163c16c53fa7b6aec5fbaf7f552dec3
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2023-09-06 07:35:45 +0530

    Add_binary.java

---
+++ Add_binary.java
@@ -0,0 +1,25 @@
+public class MyClass {
+    public static void main(String args[]) {
+        String a ="11";
+        String b="1";
+        StringBuilder out = new StringBuilder();
+        int carry=0;
+        int i = a.length()-1;
+        int j = b.length()-1;
+        System.out.println(i+" "+j);
+        while(i>=0 || j>=0 || carry!=0){
+            int digitA = i>=0 ? a.charAt(i)-'0':0;
+            int digitB = j>=0 ? b.charAt(j)-'0':0;
+            int currentSum = digitA + digitB + carry;
+            System.out.println(digitA+" "+digitB+" "+currentSum);
+            out.insert(0,currentSum%2);
+            carry = currentSum/2;
+
+            i--;
+            j--;
+            
+        }
+        System.out.println(out);
+        
+    }
+}
