9173f596c77b1c89e9f3ec360a628da0ef4bc04a
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2023-10-11 22:09:03 +0530

    Leetcode problem majority element python

---
+++ LCP_Majorityelement.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        li={}
+        for i in nums:
+            if i in li:
+                li[i]+=1
+            else:
+                li[i]=1
+        for i in li:
+            if li[i]>len(nums)/2:
+                return i
+        
