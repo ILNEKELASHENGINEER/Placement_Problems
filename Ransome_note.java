c25937d13f1f10270dcd05742745788be41c298f
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2023-10-30 22:58:49 +0530

    Ransome_note || java

---
+++ Ransome_note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int [] count = new int[26];
+        for(char ch:magazine.toCharArray()){
+            count[ch-'a']++;
+        }
+        
+        for(char ch:ransomNote.toCharArray()){
+            count[ch-'a']--;
+        
+        if(count[ch-'a']<0)
+            return false;}
+            return true;
+    
+        
+    }
+}
+//LEETCODE PROBLEM
