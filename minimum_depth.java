2c77227d40ed83c92ce961891f0509186c7c968a
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2023-11-08 21:39:18 +0530

    minimum_depth || java || Leetcode

---
+++ minimum_depth.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        if(root.left == null) return 1+minDepth(root.right);
+        if(root.right == null) return 1+minDepth(root.left);
+        return Math.min(minDepth(root.left),minDepth(root.right))+1;
+    }
+}
