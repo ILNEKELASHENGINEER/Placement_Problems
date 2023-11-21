383d317cadb9e835125ddf29e914c3c885ca6d47
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2023-11-21 22:24:58 +0530

    Create Immediate-Food-Delivery-II || sql || LEETCODE

---
+++ Immediate-Food-Delivery-II.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT 
+ROUND(100 * SUM(
+    CASE 
+        WHEN DATEDIFF(b.min_order_date,  b.min_delivery_date) = 0 THEN 1 
+        ELSE 0 
+    END
+) / COUNT(*), 2) AS immediate_percentage
+FROM (
+    SELECT 
+        MIN(order_date) AS min_order_date, 
+        MIN(customer_pref_delivery_date) AS min_delivery_date
+    FROM delivery
+    GROUP BY customer_id
+) b;
