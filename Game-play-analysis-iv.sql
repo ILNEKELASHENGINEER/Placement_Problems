82961c9633092ed1f1294cb4f62b2cb05f419dcb
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2023-11-25 21:43:48 +0530

    Game-play-analysis-iv || sql || LEETCODE 550

---
+++ Game-play-analysis-iv.sql
@@ -0,0 +1,6 @@
+select round(count(b.player_id) / count(a.player_id), 2) as fraction
+from
+(select player_id, min(event_date) as first_login from Activity
+group by 1) a
+left join Activity b
+on a.player_id = b.player_id and a.first_login = b.event_date - interval 1 day
