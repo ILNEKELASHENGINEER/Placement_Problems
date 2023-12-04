a7be6e083157a1ddc223fe209593e324620b7ca7
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2023-12-04 22:10:34 +0530

    Number-of-employees-report || sql || LEETCODE 1731

---
+++ Number-of-employees-report.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select e1.employee_id,e1.name,COUNT(e2.reports_to) as reports_count,ROUND(AVG(e2.age)) as average_age from Employees e1 
+left join Employees e2 on e1.employee_id = e2.reports_to 
+where e2.reports_to is not null
+group by e2.reports_to
+order by e1.employee_id;
