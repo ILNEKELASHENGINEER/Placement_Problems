7e11169b8065fd24c8bce1db0e2f22a0b67a9596
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2024-01-18 22:55:27 +0530

    Memoize || js || LEETCODE 2623

---
+++ memoize.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let memo = {};
+    return function(...args) {
+        const argString = JSON.stringify(args);
+        if(argString in memo){
+            return memo[argString];
+        }
+            let temp = fn(...args);
+            memo[argString] = temp;
+            return temp;
+        
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
