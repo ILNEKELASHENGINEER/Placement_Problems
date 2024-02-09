3c192e8abfbc36c4cfdd77ab02ff792e88a13832
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2024-02-09 14:25:31 +0530

    Stockmax || java || hackerrank

---
+++ stockmax.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'stockmax' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts INTEGER_ARRAY prices as parameter.
+     */
+
+    public static long stockmax(List<Integer> prices) {
+        int n = prices.size();
+long profit = 0;
+int maxPrice = prices.get(n - 1);
+long[] dp = new long[n];
+for (int i = n - 2; i >= 0; i--) {
+if (prices.get(i) < maxPrice) {
+profit += maxPrice - prices.get(i);
+dp[i]+=maxPrice - prices.get(i);
+}     else {
+maxPrice = prices.get(i);
+}
+}
+// return profit;
+return Arrays.stream(dp).sum();
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> prices = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                long result = Result.stockmax(prices);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
