27ed5ac76d2b013fa90e822e963639059ebeada9
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2024-06-14 11:26:26 +0530

    Create sp1.java

---
+++ Star_Patterns/sp1.java
@@ -0,0 +1,29 @@
+class main{
+    public static void main(String []args){
+    int n=5;
+    //INCREASING LEFT TRIANGLE 
+    //   for(int i=0;i<5;i++){
+    //       for(int j=0;j<5;j++){
+    //           System.out.print("* ");
+    //       }
+    //       System.out.println();
+    //   }
+    
+    //RIGHT LANDED PYRAMID (incr + DEC TRIANGLE)
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print("$ ");
+    //     }
+    //     System.out.println();
+    // }
+    // for(int i=n-1;i>=0;i--){
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print("$ ");
+    //     }
+    // System.out.println();
+        
+    // }
+    
+    
+    }
+}
