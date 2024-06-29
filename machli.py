2f80d57444ec1be5f827d61510545c8a744e9afa
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2024-06-29 14:34:27 +0530

    Create machli.py

---
+++ machli.py
@@ -0,0 +1,197 @@
+n=10
+li=["machli" ,"paani me gaye!","sapang"]
+for i in range(1,n+1):
+    print(i,"time\n")
+    for k in li:
+        for j in range(1,i+1):
+            if k=="machli":
+                print(i,"",k)
+            else:
+                print(k)
+# output
+
+# 1 time
+
+# 1  machli
+# paani me gaye!
+# sapang
+# 2 time
+
+# 2  machli
+# 2  machli
+# paani me gaye!
+# paani me gaye!
+# sapang
+# sapang
+# 3 time
+
+# 3  machli
+# 3  machli
+# 3  machli
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# sapang
+# sapang
+# sapang
+# 4 time
+
+# 4  machli
+# 4  machli
+# 4  machli
+# 4  machli
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# sapang
+# sapang
+# sapang
+# sapang
+# 5 time
+
+# 5  machli
+# 5  machli
+# 5  machli
+# 5  machli
+# 5  machli
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# 6 time
+
+# 6  machli
+# 6  machli
+# 6  machli
+# 6  machli
+# 6  machli
+# 6  machli
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# 7 time
+
+# 7  machli
+# 7  machli
+# 7  machli
+# 7  machli
+# 7  machli
+# 7  machli
+# 7  machli
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# 8 time
+
+# 8  machli
+# 8  machli
+# 8  machli
+# 8  machli
+# 8  machli
+# 8  machli
+# 8  machli
+# 8  machli
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# 9 time
+
+# 9  machli
+# 9  machli
+# 9  machli
+# 9  machli
+# 9  machli
+# 9  machli
+# 9  machli
+# 9  machli
+# 9  machli
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# 10 time
+
+# 10  machli
+# 10  machli
+# 10  machli
+# 10  machli
+# 10  machli
+# 10  machli
+# 10  machli
+# 10  machli
+# 10  machli
+# 10  machli
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# paani me gaye!
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
+# sapang
